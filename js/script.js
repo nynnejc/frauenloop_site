c21a6789e23103292ff5d260dabd3d5b2fe11ca1
wissam <exit>
2016-09-13 23:54:11 +0200

    creat leagal page + icon buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
-$(document).ready(function() {
-
+/*scroll down*/ 
+ $(function() {
+    $('.nav a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
   });
+ /* var yourAudio = document.getElementById('yourAudio'),
+    ctrl = document.getElementById('audioControl');
+
+    ctrl.onclick = function () {
+
+
+      if(ctrl.className == "glyphicon glyphicon-play") {
+        ctrl.className="glyphicon glyphicon-pause";
+        yourAudio["play"]();
+      }
+      else if (ctrl.className == "glyphicon glyphicon-pause"){
+
+        ctrl.className="glyphicon glyphicon-play";
+        yourAudio["pause"]();
+
+      }
+    };*/
+/*audio player*/    
+    function play(sound){
+   var audio = document.getElementById("audio");
+    audio.setAttribute('src', sound);
+   audio.play();
+}
+function stopAudio(){
+   var audio = document.getElementById("audio");
+   audio.pause();
+}
