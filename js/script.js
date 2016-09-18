97935d2c9a42ab5430e45a3b1f996ef72ebfc9cd
wissam <exit>
2016-09-18 16:01:16 +0200

    adding the new sound images

--- js/script.js
+++ js/script.js
@@ -1,44 +1,50 @@
-/*scroll down*/ 
- $(function() {
-    $('.nav a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
-    });
-  });
- /* var yourAudio = document.getElementById('yourAudio'),
-    ctrl = document.getElementById('audioControl');
 
-    ctrl.onclick = function () {
-
-
-      if(ctrl.className == "glyphicon glyphicon-play") {
-        ctrl.className="glyphicon glyphicon-pause";
-        yourAudio["play"]();
-      }
-      else if (ctrl.className == "glyphicon glyphicon-pause"){
-
-        ctrl.className="glyphicon glyphicon-play";
-        yourAudio["pause"]();
-
-      }
-    };*/
 /*audio player*/    
-    function play(sound){
-   var audio = document.getElementById("audio");
-    audio.setAttribute('src', sound);
-   audio.play();
+function play(sound){
+ var audio = document.getElementById("audio");
+ audio.setAttribute('src', sound);
+ audio.play();
 }
 function stopAudio(){
-   var audio = document.getElementById("audio");
-   audio.pause();
+ var audio = document.getElementById("audio");
+ audio.pause();
 }
 
+// Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+      target: '.navbar-fixed-top',
+      offset: 51
+    });
 
+/*swip for mobile*/
+    $(document).ready(function() {  
+
+          //Enable swiping...
+          $(".carousel-inner").swipe( {
+            //Generic swipe handler for all directions
+            swipeLeft:function(event, direction, distance, duration, fingerCount) {
+              $(this).parent().carousel('next'); 
+            },
+            swipeRight: function() {
+              $(this).parent().carousel('prev'); 
+            },
+            //Default is 75px, set to 0 for demo so any distance triggers swipe
+            threshold:0
+          });
+        });
+/*scroll down*/ 
+$(function() {
+  $('.nav a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+    
