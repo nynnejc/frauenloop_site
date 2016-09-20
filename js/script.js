fb3227750f13488237dfc0536fc5730f4aa98e4c
wissam <exit>
2016-09-20 23:56:17 +0200

    change the social icons position

--- js/script.js
+++ js/script.js
@@ -39,8 +39,8 @@ $(function() {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top 
         }, 1000);
         return false;
       }
